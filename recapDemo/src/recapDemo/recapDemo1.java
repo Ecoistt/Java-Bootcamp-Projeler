d47c8e92996b2b3ba45ef0c154f0f79bf8cf991d
Ecoistt <152798541+Ecoistt@users.noreply.github.com>
2024-02-03 00:22:24 +0300

    Java Projeler

---
+++ recapDemo/src/recapDemo/recapDemo1.java
@@ -0,0 +1,27 @@
+package recapDemo;
+
+public class recapDemo1 {
+
+	public static void main(String[] args) {
+
+		int sayi1 = 630;
+		int sayi2 = 510;
+		int sayi3 = 703;
+		int enBuyuk = sayi1;
+
+		if (enBuyuk < sayi2) {
+			enBuyuk = sayi2;
+		}
+		
+
+			if (enBuyuk < sayi3) {
+				enBuyuk = sayi3;
+				
+			}
+			System.out.println("En Büyük="+enBuyuk);
+
+		
+		}
+	
+
+}
