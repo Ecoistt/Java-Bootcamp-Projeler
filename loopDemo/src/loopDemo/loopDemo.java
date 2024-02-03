e8ad9ff3800e33ddcc5b8429762df2b5081c6ca0
Ecoistt <152798541+Ecoistt@users.noreply.github.com>
2024-02-03 16:39:48 +0300

    Java projeleri

---
+++ loopDemo/src/loopDemo/loopDemo.java
@@ -0,0 +1,15 @@
+package loopDemo;
+
+public class loopDemo {
+
+	public static void main(String[] args) {
+		// 100 e kadar olan tek sayıları ekrana yazdır.
+		
+		for(int i=1; i<=100; i+=2) {
+			System.out.println(i);
+		}
+		System.out.println("Döngü Bitti.");
+
+	}
+
+}
